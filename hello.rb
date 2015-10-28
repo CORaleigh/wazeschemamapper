87f7bdcbef8a499c50e99f6306ca8c6062b77879
Chad Foley <chad.foley@raleighnc.gov>
2015-10-27 22:32:21 -0400

    test consuming json from arcgic

--- hello.rb
+++ hello.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   "Hello World!"
@@ -6,6 +7,10 @@ end
 
 get '/getEventData' do
   content_type 'application/json'
+  corevents = open("http://maps.raleighnc.gov/arcgis/rest/services/SpecialEvents/SpecialEventsView/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson").read
+  corevents = JSON.parse(corevents)
+  puts "get event name..."
+  puts corevents['EVENT_NAME']
   puts "consume arcgis endpoint and map response to waze format"
   return "testing 123"
 end
