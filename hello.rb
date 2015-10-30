b48bd8bbe91504e0b8fbb520ed313ff7710e19d7
Chad Foley <chad.foley@raleighnc.gov>
2015-10-30 16:44:58 -0400

    added lat/lng conversion outSR=4326

--- hello.rb
+++ hello.rb
@@ -6,8 +6,13 @@ get '/' do
 end
 
 get '/getEventData' do
-	response = HTTParty.get('http://maps.raleighnc.gov/arcgis/rest/services/SpecialEvents/SpecialEventsView/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson')
+	response = HTTParty.get('http://maps.raleighnc.gov/arcgis/rest/services/SpecialEvents/SpecialEventsView/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=json')
 	puts response.body, response.code, response.message, response.headers.inspect
+
+	# convert to waze schema
+
 	return response.body
 end
 
+
+get '/getAccidentClosureData'
